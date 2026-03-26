772d3a140cf62abbf8231fa06de277b60b7bc253
Ivyyuer <a0903808507@gmail.com>
2026-03-26 17:58:29 +0800

    feat: main page update

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,7 +30,47 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <div className="flex h-full">
+        {/*左邊*/}
+        <div className="bg-white-200 w-[320px] h-full p-4">
+
+          <div className="flex justify-center items-center w-full"></div>
+          <div className="bg-gray-200 w-[80px] h-[80px] rounded-full overflow-hidden flex justify-center items-center">
+            <Image src="/hiiiiiiii拷貝.png" alt="main" width={80} height={80}/>
+          </div>
+
+          <div className="text-center">名字</div>
+          <div>簡介</div>
+
+          <div className="flex gap-2">
+            {/*icons, links*/}
+            <div>社群連結</div>
+            <div>社群連結</div>
+            <div>社群連結</div>
+          </div>
+          
+          
+
+          <Link href="/about">
+            <div className="bg-gray-300 p-[16px] rounded-md">關於我</div>
+          </Link>
+          <Link href="/hobby">
+          <div className="bg-gray-300 p-[16px] mt-2 rounded-md">我的興趣</div>
+          </Link>
+          
+          <div className="bg-gray-300 p-[16px] mt-2 rounded-md">設計專案</div>
+          <div className="bg-gray-300 p-[16px] mt-2 rounded-md">遊戲專案</div>
+          <div className="bg-gray-300 p-[16px] mt-2 rounded-md">Python專案</div>
+
+          
+
+        </div>
+        <div className="bg-white-200 w-full h-full">
+          {children}
+        </div>
+      </div>
+      </body>
     </html>
   );
 }
